46fe5c68fc6e59f68aee96c6e207dc7c0c4d4acb
shanfenga <tanghl1203@163.com>
2023-09-19 13:48:21 +0800

    test

---
+++ Source/myjob/myjobGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "myjobGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AmyjobGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AmyjobGameMode();
+};
+
+
+
